14bda9f07caa24dc84dd8f68245039622380072e
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-06 18:34:07 +0300

    Add: sqlite initialized.

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -21,7 +21,10 @@ impl Application {
                 .build(),
             db_service: match MainDb::new() {
                 Ok(initialized) => Some(Box::new(initialized)),
-                _ => None,
+                _ => {
+                    println!("Database service could not be initialized.");
+                    None
+                },
             },
         }
     }
@@ -67,11 +70,35 @@ struct MainDb {
 }
 
 impl MainDb {
+    fn create_new_db(name: &str, flags: sqlite::OpenFlags) -> sqlite::Result<sqlite::Connection> {
+        let connection = sqlite::Connection::open_with_flags(name, flags.set_create())?;
+        connection.execute("
+            CREATE TABLE users (
+                user_id int,
+                first_name varchar(255),
+                last_name varchar(255)
+            );
+        ")?;
+        Ok(connection)
+    }
+
     fn new() -> sqlite::Result<MainDb> {
-        let conn = sqlite::open(":memory:");
+        let flags = sqlite::OpenFlags::new()
+            .set_read_write()
+            .set_full_mutex();
+        let dbname = "appdb.sqlite";
+
+        let conn = match sqlite::Connection::open_with_flags(&dbname, flags.clone()) {
+            Ok(conn) => Ok(conn),
+            _ => MainDb::create_new_db(&dbname, flags)
+        };
         match conn {
             Ok(conn) => Ok(MainDb { connection: conn }),
-            Err(error) => Err(error),
+            Err(error) => {
+                println!("Could not initialize connection.");
+                println!("Reason : {}", error.message.clone().unwrap());
+                Err(error)
+            },
         }
     }
 }
