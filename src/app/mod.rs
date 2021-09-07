1f233a739e316292ef345e8178d1b85a159eefec
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-07 09:54:55 +0300

    Add: database user fields (columns).

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -76,7 +76,10 @@ impl MainDb {
             CREATE TABLE users (
                 user_id int,
                 first_name varchar(255),
-                last_name varchar(255)
+                last_name varchar(255),
+                email varchar(255),
+                username varchar(255),
+                password varchar(255)
             );
         ")?;
         Ok(connection)
