c51d1fcad0dfe83a2063484dea7f9c53703098b1
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-07 10:10:16 +0300

    Add: user model fields.

--- src/app/models.rs
+++ src/app/models.rs
@@ -1,5 +1,12 @@
-pub struct User;
+pub struct User {
+    first_name: String,
+    last_name: String,
+    id: String,
+    email: String,
+    password: String,
+    salt: String
+}
+
 pub struct Task;
 pub struct Board;
 pub struct SessionId(String);
-pub struct SignUpForm(String, String);
