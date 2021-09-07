5c0ee40b31bf84c58bb653804d1087630ebfa122
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-07 09:59:52 +0300

    Add: user's password salt.

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -79,7 +79,8 @@ impl MainDb {
                 last_name varchar(255),
                 email varchar(255),
                 username varchar(255),
-                password varchar(255)
+                password varchar(255),
+                salt varchar(255)
             );
         ")?;
         Ok(connection)
