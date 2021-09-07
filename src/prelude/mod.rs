be9bffc2988d629ca9d8a0d94fa5b9de2a6ce30b
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-07 11:35:10 +0300

    Add: submittable form trait.

--- src/prelude/mod.rs
+++ src/prelude/mod.rs
@@ -1,7 +1,13 @@
+use crate::app::Error;
+
 pub trait Builder<T> {
     fn build(&self) -> T;
 }
 
 pub trait View {
     fn assemble(&self) -> gtk::Widget;
+}
+
+pub trait Form<T> {
+    fn submit(&self) -> Result<T, Error>;
 }
