b52ec4177e10557c046cf8e8005eded647a1c28a
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-07 11:36:09 +0300

    Add: SignUpForm partial implementation.

--- src/app/models.rs
+++ src/app/models.rs
@@ -1,10 +1,43 @@
+use crate::app::Error;
+use crate::prelude::Form;
+
+#[derive(Debug)]
+pub struct SignUpForm {
+    first_name: String,
+    last_name: String,
+    email: String,
+    username: String,
+    password: String,
+    on_submit: fn() -> Result<User, Error>,
+}
+
+impl SignUpForm {
+    pub fn new(submit_action: fn() -> Result<User, Error>) -> Self {
+        SignUpForm {
+            on_submit: submit_action,
+            first_name: Default::default(),
+            last_name: Default::default(),
+            email: Default::default(),
+            username: Default::default(),
+            password: Default::default()
+        }
+    }
+}
+
+impl Form<User> for SignUpForm {
+    fn submit(&self) -> Result<User, Error> {
+        (self.on_submit)()
+    }
+}
+
+#[derive(Default,Debug)]
 pub struct User {
     first_name: String,
     last_name: String,
     id: String,
     email: String,
     password: String,
-    salt: String
+    salt: String,
 }
 
 pub struct Task;
