0cbac25bc6fa84d5260e088cccc557cf4eca4ed9
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-08 13:28:02 +0300

    Add: injecting services from main.

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,16 @@ mod root;
 use crate::prelude::*;
 use gtk::prelude::*;
 
+use crate::app::{MainDb, UserManager};
+
 fn main() {
     gtk::init().expect("Could not initialize GTK");
 
-    let tm = Box::new(app::Application::new());
+    let db = MainDb::new().unwrap();
+    let us = UserManager::new().database(&db);
+    let tm = Box::new(app::Application::new()
+        .database(&db)
+        .user_service(&us));
 
     tm.run();
 }
