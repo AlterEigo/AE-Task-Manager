a778e0205cd9768a33e170dbcf12bfa556cc49e9
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-08 12:42:42 +0300

    Add: RootView now routes user service to AuthView.

--- src/root.rs
+++ src/root.rs
@@ -1,23 +1,25 @@
 use gtk::prelude::*;
 
+use crate::app::services::UserService;
 use crate::auth::AuthView;
 use crate::prelude::View;
 
-pub struct RootView;
-impl RootView {
-    pub fn new() -> Self {
-        RootView {}
-    }
+pub struct RootView<'a> {
+    user_srv: &'a dyn UserService
+}
 
-    fn assemble_auth() -> gtk::Widget {
-        AuthView::new().assemble()
+impl<'a> RootView<'a> {
+    pub fn new(user_srv: &'a dyn UserService) -> Self {
+        RootView {
+            user_srv: user_srv
+        }
     }
 }
 
-impl View for RootView {
+impl<'a> View for RootView<'a> {
     fn assemble(&self) -> gtk::Widget {
         let grid = gtk::Grid::builder().build();
-        let auth = RootView::assemble_auth();
+        let auth = AuthView::new(self.user_srv);
         grid.attach(&auth, 0, 0, 1, 1);
         grid.show();
         grid.dynamic_cast::<gtk::Widget>().unwrap()
