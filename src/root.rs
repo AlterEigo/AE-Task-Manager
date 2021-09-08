53cbeffa16c37a1712926f05ff167b6013bf1ef2
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-08 13:17:55 +0300

    Add: UserService injection into root.

--- src/root.rs
+++ src/root.rs
@@ -4,14 +4,20 @@ use crate::app::services::UserService;
 use crate::auth::AuthView;
 use crate::prelude::View;
 
+#[derive(Default)]
 pub struct RootView<'a> {
-    user_srv: &'a dyn UserService
+    user_srv: Option<&'a dyn UserService>
 }
 
 impl<'a> RootView<'a> {
-    pub fn new(user_srv: &'a dyn UserService) -> Self {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn user_service(self, value: &'a dyn UserService) -> Self {
         RootView {
-            user_srv: user_srv
+            user_srv: Some(value),
+            ..self
         }
     }
 }
@@ -19,7 +25,9 @@ impl<'a> RootView<'a> {
 impl<'a> View for RootView<'a> {
     fn assemble(&self) -> gtk::Widget {
         let grid = gtk::Grid::builder().build();
-        let auth = AuthView::new(self.user_srv);
+        let auth = AuthView::new()
+            .user_service(self.user_srv.unwrap())
+            .assemble();
         grid.attach(&auth, 0, 0, 1, 1);
         grid.show();
         grid.dynamic_cast::<gtk::Widget>().unwrap()
