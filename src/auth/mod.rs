a3e28fea44e4586b1e6fbdc58ae0abbbd35d7f90
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-09-08 16:18:38 +0300

    Mod: AuthView now store a shared ptr to its service.

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,35 +1,28 @@
-use crate::prelude::View;
+use crate::prelude::*;
 use gtk::prelude::*;
+use std::rc::Rc;
 
 use crate::app::services::UserService;
 
-#[derive(Default, Clone, Copy)]
-pub struct AuthView<'a> {
-    user_srv: Option<&'a dyn UserService>,
+pub struct AuthView {
+    user_srv: Option<Rc<dyn UserService>>,
 }
 
-impl<'a> AuthView<'a> {
+impl AuthView {
     pub fn new() -> Self {
         AuthView {
-            ..Default::default()
+            user_srv: None
         }
     }
 
-    pub fn user_service(self, value: &'a dyn UserService) -> Self {
+    pub fn user_service(self, value: Rc<dyn UserService>) -> Self {
         AuthView {
             user_srv: Some(value),
             ..self
         }
     }
 
-    fn action_sign_in(btn: &gtk::Button, username: &str, password: &str) {
-        println!(
-            "Submitted username: '{}' password: '{}'",
-            username, password
-        )
-    }
-
-    fn bind_inputs(builder: &gtk::Builder) {
+    fn bind_inputs(&self, builder: &gtk::Builder) {
         let (e_login, e_password, b_signin) = (
             builder.object::<gtk::Entry>("e_username").unwrap(),
             builder.object::<gtk::Entry>("e_password").unwrap(),
@@ -43,27 +36,33 @@ impl<'a> AuthView<'a> {
         e_password.connect_activate(move |_| btn.emit_clicked());
     }
 
-    fn bind_buttons(builder: &gtk::Builder) {
-        let (e_login, e_password, b_signin) = (
+    fn bind_buttons(&self, builder: &gtk::Builder) {
+        let (e_login, e_password, b_signin)= (
             builder.object::<gtk::Entry>("e_username").unwrap(),
             builder.object::<gtk::Entry>("e_password").unwrap(),
             builder.object::<gtk::Button>("b_signin").unwrap(),
         );
 
-        b_signin.connect_clicked(move |btn| {
-            let username = String::from(e_login.text());
-            let password = String::from(e_password.text());
-            AuthView::action_sign_in(btn, &username, &password);
-        });
+        if let Some(srv) = self.user_srv {
+            let user_srv = Rc::clone(&srv);
+            b_signin.connect_clicked(move |btn| {
+                let username = String::from(e_login.text());
+                let password = String::from(e_password.text());
+                if let Ok(form) = user_srv.sign_up() {
+                    form.username(username).password(password).submit();
+                }
+                // self.action_sign_in(btn, &username, &password);
+            });
+        }
     }
 }
 
-impl<'a> View for AuthView<'a> {
+impl View for AuthView {
     fn assemble(&self) -> gtk::Widget {
         let builder = gtk::Builder::from_resource("/org/altereigo/ae-task-manager/AuthFrame.glade");
 
-        AuthView::bind_inputs(&builder);
-        AuthView::bind_buttons(&builder);
+        self.bind_inputs(&builder);
+        self.bind_buttons(&builder);
         let grid: gtk::Grid = builder.object("root").unwrap();
         grid.show();
         grid.dynamic_cast::<gtk::Widget>().unwrap()
